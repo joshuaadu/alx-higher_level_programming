9a60c1f0dc576ff8cb6a92816e4b94e8c859a3fb
Joshua Adu-Mensah <joshuaadu2001@gmail.com>
2022-12-08 05:08:13 +0000

    update

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+arg_len = len(sys.argv)
+if arg_len == 2:
+    print("{:d} argument".format(arg_len - 1))
+    print("{:d}: {:s}".format(1, sys.argv[1]))
+elif arg_len > 2:
+    print("{:d} arguments".format(arg_len - 1))
+    for i in range(1, arg_len):
+        print("{:d}: {:s}".format(i, sys.argv[i]))
+else:
+    print("0 arguments")
