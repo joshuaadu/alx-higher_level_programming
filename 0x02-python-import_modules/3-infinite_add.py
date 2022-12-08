ff7d6f328d8824592cd9ba4dba4bd124d72126bb
Joshua Adu-Mensah <joshuaadu2001@gmail.com>
2022-12-08 16:33:15 +0000

    update

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    arg_len = len(sys.argv)
+    sum = 0
+    if arg_len > 1:
+        for i in range(1, arg_len):
+            sum += int(sys.argv[i])
+    print(sum)
