3c73d19b238273dd6acabef10325ea316008b897
Joshua Adu-Mensah <joshuaadu2001@gmail.com>
2022-12-12 09:28:31 +0000

    prints a dictionary by ordered keys

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def print_sorted_dictionary(a_dictionary):
+    """prints a dictionary by ordered keys"""
+    # get list of keys
+    keys = list(a_dictionary)
+    # sort keys
+    keys.sort()
+    # loop through dict and print out each item
+    for key in keys:
+        print("{:s}: {:s}".format(key, str(a_dictionary[key])))
