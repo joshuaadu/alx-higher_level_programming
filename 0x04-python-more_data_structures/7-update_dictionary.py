6ccd20275d863416675577688be31bc53564f889
Joshua Adu-Mensah <joshuaadu2001@gmail.com>
2022-12-13 13:28:12 +0000

    update

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -4,6 +4,6 @@ def update_dictionary(a_dictionary, key, value):
     if key in a_dictionary:
         a_dictionary[key] = value
     else:
-        a_dictionary |= [(key, value)]
+        a_dictionary |= {key: value}
     # a_dictionary.update([(key, value)])
     return a_dictionary
