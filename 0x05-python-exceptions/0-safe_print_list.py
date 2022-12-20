fa86e098cdcf282bf415a113106562424e737f1a
Joshua Adu-Mensah <joshuaadu2001@gmail.com>
2022-12-20 07:59:59 +0000

    prints x elements of a list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    try:
-        num = 0
-        for i in range(x):
+    num = 0
+    for i in range(x):
+        try:
             num += 1
             print(my_list[i], end="")
-    except Exception:
-        raise
-    finally:
-        print("")
-        return num
+        except Exception:
+            break
+    print("")
+    return num
