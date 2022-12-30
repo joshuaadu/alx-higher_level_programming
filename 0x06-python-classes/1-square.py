215dc3230f2b1fd035cbb090642d100c63846c8f
Joshua Adu-Mensah <joshuaadu2001@gmail.com>
2022-12-30 22:31:48 +0000

    document modules

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Define a class Square."""
 class Square:
     """Defines a square with with a size attribute"""
     def __init__(self, size) -> None:
--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""Define a class Square."""
+
 class Square:
     """Defines a square with with a size attribute of type integer
     and is greater than 0"""
