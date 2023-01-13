d6cf6efa2802c66281e9912f3ea3865d9c996f45
Joshua Adu-Mensah <joshuaadu2001@gmail.com>
2023-01-13 22:23:26 +0000

    update

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -10,8 +10,8 @@ class Rectangle():
     Create a Rectangle class
     """
     def __init__(self, width=0, height=0):
-        self.__height = height
-        self.__width = width
+        self.height = height
+        self.width = width
 
     @property
     def width(self):
