f679296af5bb2f842d69c147b08aaa5dde757bb7
Joshua Adu-Mensah <joshuaadu2001@gmail.com>
2023-01-13 07:59:56 +0000

    update

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+# 0-rectangle.py
+"""
+This module defines a Rectangle Class
+"""
+
+
+class Rectangle():
+    """
+    Create a Rectangle class
+    """
+    def __init__(self, width=0, height=0):
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        """
+        Returns the width of the rectangle
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        Sets the width of the rectangle
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """
+        Gets the height of the rectangle
+        """
+        return self.__width
+
+    @height.setter
+    def height(self, value):
+        """
+        Sets the height of the rectangle
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """
+        Returns the rectangle area
+        """
+        return self.__height * self.__width
+
+    def perimeter(self):
+        """
+        Returns the rectangle perimeter
+        """
+        if self.__width == 0 or self.height == 0:
+            return 0
+        else:
+            return (2 * self.__height) + (2 * self.__width)
+
+
+
+if __name__ == "__main__":
+    rect1 = Rectangle()
+    rect1 = Rectangle(1, 2)
+    rect1 = Rectangle(2, 3)
+    print(rect1.__class__)
+    
