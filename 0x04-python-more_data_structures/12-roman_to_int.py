86d5fd1973af03975b3b741e04bc822d63e3048d
joshuaadu <joshuaadu2001@gmail.com>
2023-01-17 15:45:17 +0000

    update

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
-def roman_to_int(roman_string):
-    
+def roman_to_int(roman_string: str):
+    numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500,
+                'M': 1000}
+    cap_strings = roman_string.upper()
+    int_result = None
+    numerals.keys
+    for x in roman_string.capitalize():
+        
