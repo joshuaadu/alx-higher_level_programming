1a39e7b9e5222d47725022ebfcb33a4efcb55e9a
joshuaadu <joshuaadu2001@gmail.com>
2023-01-23 10:26:51 +0000

    update

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -8,3 +8,15 @@ class MyList(list):
     """Implements a sorted printing for the built-in list class"""
     def print_sorted(self):
         print(sorted(self))
+
+
+if __name__ == "__main__":
+    my_list = MyList()
+    my_list.append(1)
+    my_list.append(4)
+    my_list.append(2)
+    my_list.append(3)
+    my_list.append(5)
+    print(my_list)
+    my_list.print_sorted()
+    print(my_list)
